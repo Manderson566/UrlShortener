add44efb0c7bf88b48604f7f1236968666f4ca50
Michael Anderson <Micmail566@gmail.com>
2017-02-13 14:03:27 -0500

    Updated database with bookmark model and click model.

---
+++ UrlShortener/Models/Click.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace UrlShortener.Models
+{
+    public class Click
+    {
+        public int Id { get; set; }
+        public DateTime Created { get; set; }
+
+        public string BookmarkId { get; set; }
+
+        [ForeignKey("BookmarkId")]
+        public virtual Bookmark Bookmark { get; set; }
+
+        public string OwnerId { get; set; }
+
+        [ForeignKey("OwnerId")]
+        public virtual ApplicationUser Owner { get; set; }
+    }
+}
